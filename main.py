84b521b8cb77e6b8fb12700cdfdcc2f3d8b81aca
shohruzbekturov <shohruz.bekturov@gmail.com>
2026-08-13 18:35:00 +0500

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ db.commit()
 
 
 
-bot = Bot(token=os.getenv("BOT_TOKEN"))
+bot = Bot(token=os.getenv("8940343469:AAHYG6SAppqBy43oPB0CKwRxdOk2uIcdQUc"))
 dp = Dispatcher()
 
 class Reviews(StatesGroup):
